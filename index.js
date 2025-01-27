1c9877b66b3c3bd2db0928727546c1f14d1b4be7
mdsayedadnan <sayedadnan6441@gmail.com>
2025-01-27 20:06:21 +0600

    scend commit

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ytced.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zyfftle.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
